a31bb29f7dbf06d585dc114704ae206f7bacb49e
Fan Yang <fan.yang.turner@live.co.uk>
2018-11-29 16:36:21 +0000

    Update dockerfile and app

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+    html = "<h3>Hello {name}! This is a Test App</h3>" \
+           "<b>Running at:</b> {hostname}<br/>"
+    return html.format(name="World", hostname=socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
