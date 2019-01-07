a19c098cebbf547411c866afa500be3666047a00
oxfordfun <fan.yang-turner@ndm.ox.ac.uk>
2019-01-07 14:38:48 +0000

    Add hello name endpoint

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from datetime import datetime
 import os
 import socket
+import re
 
 app = Flask(__name__)
 
@@ -10,6 +12,23 @@ def hello():
            "<b>Hostname:</b> {hostname}<br/>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
 
