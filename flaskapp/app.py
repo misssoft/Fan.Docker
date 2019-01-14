4eead59768e74a5fb6b9e4783f5b806a1d5ad2c5
Fan Yang <fan.yang-turner@ndm.ox.ac.uk>
2019-01-14 14:58:01 +0000

    Add upload file page

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,10 +1,39 @@
 from flask import Flask, render_template
+from flask import flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from datetime import datetime
 import os
 import socket
 import re
 
+UPLOAD_FOLDER = 'D:\\tmp\\uploads'
+ALLOWED_EXTENSIONS = set(['csv','tsv','txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('upload.template', filename = filename)
+    return render_template('upload.template')
 
 @app.route("/")
 def hello():
